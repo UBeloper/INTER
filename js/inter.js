1ab49709c392e8fec5aedcddf1cc1872563ad8b2
UB <theurain@naver.com>
2021-12-11 22:52:59 +0900

    1

--- js/inter.js
+++ js/inter.js
@@ -23,3 +23,19 @@ function dropnews() {
     dabout.style.display="none";
     dnews.style.display ="block";
 }
+
+/* 메인페이지 autoslide */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = "0";    
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.opacity = "1";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
