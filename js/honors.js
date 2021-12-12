b121fc7b0001a9ccf3d3b83fb1ef8396a87b9526
UB <theurain@naver.com>
2021-12-12 12:13:38 +0900

    sun

--- js/honors.js
+++ js/honors.js
@@ -1,16 +1,3 @@
-/* 인트로 - 아무곳이나 클릭하면 메인화면으로  */
-let videointro = document.getElementById("videointro");
-
-function linkmain() {
-    window.onclick = function(event) {
-        if(event.target == videointro) {
-             videointro.style.opacity = "0";
-            videointro.style.transition = "all 2s";
-        }
-        videointro.style.zIndex = "-1";  
-    }
-}
-
 /* 메뉴 클릭시 드랍다운 */
 let dabout = document.querySelector(".drop-content-about");
 let dnews = document.querySelector(".drop-content-news");
@@ -22,4 +9,20 @@ function dropabout() {
 function dropnews() {
     dabout.style.display="none";
     dnews.style.display ="block";
+}
+
+/* 드랍다운창 끄기 */
+function closer() {
+    dabout.style.display ="none";
+    dnews.style.display="none";
+}
+
+// 로그인폼 + modal
+
+var modal = document.querySelector(".modal");
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
 }
